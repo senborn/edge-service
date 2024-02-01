52d1394076b57352a9ecafae55b5c1cd1e4a6aba
Dejan Senborn <dejan.senborn@crealogix.com>
2024-02-01 18:48:27 +0100

    Chapter11 finished

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -1,14 +1,17 @@
 package com.polarbookshop.edgeservice.config;
 
-import reactor.core.publisher.Mono;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
     @Bean
-    public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous");
+    KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
